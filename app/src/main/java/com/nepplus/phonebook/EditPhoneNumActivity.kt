0f5fd864c082ca09fd9834ddb571776f4d67ef7d
qorvhfl080 <86284854+qorvhfl080@users.noreply.github.com>
2021-09-02 17:37:38 +0900

    날짜 선택 팝업 띄우기

--- app/src/main/java/com/nepplus/phonebook/EditPhoneNumActivity.kt
+++ app/src/main/java/com/nepplus/phonebook/EditPhoneNumActivity.kt
@@ -1,7 +1,10 @@
 package com.nepplus.phonebook
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.DatePicker
+import kotlinx.android.synthetic.main.activity_edit_phone_num.*
 
 class EditPhoneNumActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,6 +13,19 @@ class EditPhoneNumActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        selectBirthDayBtn.setOnClickListener {
+
+            val dataSetListener = object : DatePickerDialog.OnDateSetListener {
+                override fun onDateSet(p0: DatePicker?, year: Int, month: Int, day: Int) {
+
+                }
+            }
+
+            val datePickerDialog = DatePickerDialog(mContext, dataSetListener, 2021, 8, 2)
+
+            datePickerDialog.show()
+        }
 
     }
 
