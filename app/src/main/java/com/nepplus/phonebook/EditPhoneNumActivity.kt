78d1078d63ed0ab9542b72919be55f7caccd4676
qorvhfl080 <86284854+qorvhfl080@users.noreply.github.com>
2021-09-02 17:09:22 +0900

    폰번 작성 화면으로 이동

---
+++ app/src/main/java/com/nepplus/phonebook/EditPhoneNumActivity.kt
@@ -0,0 +1,19 @@
+package com.nepplus.phonebook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditPhoneNumActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_phone_num)
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
--- app/src/main/java/com/nepplus/phonebook/MainActivity.kt
+++ app/src/main/java/com/nepplus/phonebook/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.nepplus.phonebook
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,6 +20,13 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        addPhoneNumBtn.setOnClickListener {
+
+            val myIntent = Intent(mContext, EditPhoneNumActivity::class.java)
+            startActivity(myIntent)
+
+        }
 
     }
 
